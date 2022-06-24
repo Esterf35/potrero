02e410da042d9715b34bb043232e25c2036d29ce
Esterf35 <104461428+Esterf35@users.noreply.github.com>
2022-06-24 19:04:48 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -3,11 +3,11 @@
 function copy() {alert("by ESTER FERRARI")}
 
 function fondoblack(){
-  
+
         document.body.style.backgroundColor = "black";
 
     }
 
-    function copy() {
+  function copy() {
         alert("Autora:ESTER FERRARI. EMAIL: esterf24@gmail.com");
         }
